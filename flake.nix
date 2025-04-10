486c576c28c8d8429f8c2b475ed456c3d3e79021
Jean-François Roche <jfroche@pyxel.be>
2025-04-10 14:46:54 +0200

    Initial import

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "A garnix module for Uptime Kuma";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.garnix-lib.url = "github:jfroche/garnix-lib/jfroche/expose-module-for-system";
+
+  outputs = { self, nixpkgs, garnix-lib, }:
+    let
+      lib = nixpkgs.lib;
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      uptimeKumaSubmodule.options = {
+        port = lib.mkOption {
+          type = lib.types.port;
+          description = "The port in which to run uptime-kuma";
+          default = 3001;
+        };
+      };
+    in {
+      garnixModules.default = { pkgs, config, ... }: {
+        options = {
+          uptimeKuma = lib.mkOption {
+            type = lib.types.submodule uptimeKumaSubmodule;
+            description = "An attrset of uptime-kuma instances";
+          };
+        };
+
+        config = {
+          nixosConfigurations.default = [{
+            services.uptime-kuma = {
+              enable = true;
+              settings.UPTIME_KUMA_PORT = toString config.uptimeKuma.port;
+            };
+            garnix.server.persistence = {
+              enable = true;
+              name = "uptimeKuma";
+            };
+            services.nginx = {
+              enable = true;
+              recommendedProxySettings = true;
+              recommendedOptimisation = true;
+              virtualHosts.default = {
+                locations."/".proxyPass =
+                  "http://localhost:${toString config.uptimeKuma.port}";
+              };
+            };
+            networking.firewall.allowedTCPPorts = [ 80 ];
+          }];
+        };
+      };
+      checks.x86_64-linux.default = pkgs.testers.runNixOSTest ({ lib, ... }: {
+        name = "uptime-kuma";
+        nodes.default = { lib, pkgs, ... }:
+          let
+            evaledGarnixModuleConfig = (garnix-lib.lib.evaledModulesForSystems {
+              modules = [ self.garnixModules.default ({ }) ];
+              config = { uptimeKuma.port = 8001; };
+            }).x86_64-linux;
+          in {
+            imports = [ garnix-lib.nixosModules.garnix ]
+              ++ evaledGarnixModuleConfig.config.nixosConfigurations.default;
+            garnix.server.isVM = true;
+            garnix.server.enable = true;
+          };
+        testScript = { nodes, ... }: ''
+          start_all()
+
+          default.wait_for_unit("multi-user.target")
+          default.wait_for_unit("uptime-kuma.service")
+          default.wait_for_unit("nginx.service")
+
+          default.wait_until_succeeds("curl --fail http://127.0.0.1/dashboard", 20)
+        '';
+      });
+    };
+}
